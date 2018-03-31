dddb40d8812b75ca164d2503ae00878d385f291a
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-31 20:02:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,7 +20,7 @@ end
 
 def kesha_maker(strings)
   new_names = []
-  strings.each do |string| new_names << string.insert()
+  strings.each do |string| new_names << string.insert(2, "$")
   end
   new_names
 end
