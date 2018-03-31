7dbd13e0914e1e172b70a3af0b0592f893d5cc69
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-31 21:42:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,8 +32,8 @@ def sum_array(addition)
 end
 
 def add_s(words)
-  words.each_with_index.collect do |word| 
-    if words [1] == word
+  words.collect do |word|
+  if word[1] == word
       word
     else
       word + "s"
